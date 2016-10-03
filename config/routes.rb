47291d58ebf1083d012d5541a1a73d1f6c4f5d5b
i-holod <holod@iholod.ru>
2016-10-03 23:33:56 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'pages#home'
+
+  resources :groups do
+    resources :students
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
